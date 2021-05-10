fd5766e9052cc05874c010ad3237270e7f07d54e
remote-torrent-6747 <abbyc@sky.com>
2021-05-10 19:17:34 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -20,6 +20,14 @@ class TweetsController < ApplicationController
 
 
 
-  
+  Helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
 
 end
